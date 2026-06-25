f71f38ad7e4e1da05859691ddcd74d311b9e24a8
RameesV <rameesmuhammed957@gmail.com>
2026-06-25 15:54:39 +0530

    updated footer text

--- src/components/bottom_nav.jsx
+++ src/components/bottom_nav.jsx
@@ -1,16 +1,11 @@
 function BottomNav() {
     return (
         <div className="h-[64px] flex items-center justify-center bg-[#1F4745]">
-            <p className="text-[14px] lg:text-[16px] font-normal font-inter text-center text-[#FFFFFF] leading-[22px] lg:leading-[24px]">
-              © 2025 All Right Reserved | Powered by
-                <a 
-                    href="https://mentecode.com/" 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                    className="text-[#FFFFFF] hover:text-[#FFFFFF99] pl-1"
-                >
-                  Mentecode
-                </a>
+            <p
+              className="text-[14px] lg:text-[16px] font-normal font-inter text-center text-[#FFFFFF] leading-[22px] lg:leading-[24px]"
+              dir="rtl"
+            >
+              شركة الصحة النجمية الطبية
             </p>
         </div>
     )
